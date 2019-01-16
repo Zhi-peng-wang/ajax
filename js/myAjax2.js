5db5c8dfc27176db1813e796c9b736c196519e70
wzp <742349899@qq.com>
2019-01-16 17:51:46 +0800

    更新

--- js/myAjax2.js
+++ js/myAjax2.js
@@ -1,21 +1,26 @@
-function obj2str(obj) {
+function obj2str(data) {
     /*
-               "userName": "wanzghipeng",
-                "userPwd": "123"
-                "t": "121354654864"
-     */
-    // obj.t = new Date().getTime();
-    var res = [];
-    for (var key in obj){
-        res.push(encodeURIComponent(key)+"="+encodeURIComponent(obj[key]));//[userName=wanzghipeng, userPwd=123]
+    {
+        "userName":"wzp",
+        "userPwd":"123456",
+        "t":"3712i9471329876498132"
     }
-    // 通过此方法将，转换成&
-    return res.join("&");//userName=wanzghipeng&userPwd=123
+    */
+    data = data || {}; // 如果没有传参, 为了添加随机因子,必须自己创建一个对象
+    // data.t = new Date().getTime();
+    var res = [];
+    for(var key in data){
+        // 在URL中是不可以出现中文的, 如果出现了中文需要转码
+        // 可以调用encodeURIComponent方法
+        // URL中只可以出现字母/数字/下划线/ASCII码
+        res.push(encodeURIComponent(key)+"="+encodeURIComponent(data[key])); // [userName=lnj, userPwd=123456];
+    }
+    return res.join("&"); // userName=lnj&userPwd=123456
 }
-function ajax(type, url, obj, timeout, sucess, error) {
-    //0.将传过来的obj转换成字符串
-    var str = obj2str(obj);//key=value&key2=value2
-    //1.创建一个异步对象
+function ajax(option) {
+    // 0.将对象转换为字符串
+    var str = obj2str(option.data); // key=value&key=value;
+    // 1.创建一个异步对象
     var xmlhttp, timer;
     if (window.XMLHttpRequest)
     {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -25,54 +30,52 @@ function ajax(type, url, obj, timeout, sucess, error) {
     {// code for IE6, IE5
         xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     }
-    //2.设置请求方式和请求地址
+    // 2.设置请求方式和请求地址
     /*
-        method：请求的类型；GET 或 POST
-        url：文件在服务器上的位置
-        async：true（异步）或 false（同步）
-     */
-    if (type === "GET"){
-        xmlhttp.open(type, url+"?"+str, true);
-        //3.发送请求
+    method：请求的类型；GET 或 POST
+    url：文件在服务器上的位置
+    async：true（异步）或 false（同步）
+    */
+    if(option.type.toLowerCase() === "get"){
+        xmlhttp.open(option.type, option.url+"?"+str, true);
+        // 3.发送请求
         xmlhttp.send();
-    } else {
-        xmlhttp.open(type, url, true);
+    }else{
+        xmlhttp.open(option.type, option.url,true);
+        // 注意点: 以下代码必须放到open和send之间
         xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-        //3.发送请求
         xmlhttp.send(str);
     }
 
-    //4.监听状态的变化
-    xmlhttp.onreadystatechange = function () {
+    // 4.监听状态的变化
+    xmlhttp.onreadystatechange = function (ev2) {
         /*
-            0: 请求未初始化
-            1: 服务器连接已建立
-            2: 请求已接收
-            3: 请求处理中
-            4: 请求已完成，且响应已就绪
+        0: 请求未初始化
+        1: 服务器连接已建立
+        2: 请求已接收
+        3: 请求处理中
+        4: 请求已完成，且响应已就绪
         */
-        if (xmlhttp.readyState === 4){
+        if(xmlhttp.readyState === 4){
             clearInterval(timer);
-            //判断状态码即请求是否成功
-            if (xmlhttp.status >= 200&&xmlhttp.status <300 || xmlhttp.status ===304){
-                //5.处理返回的结果
+            // 判断是否请求成功
+            if(xmlhttp.status >= 200 && xmlhttp.status < 300 ||
+                xmlhttp.status === 304){
+                // 5.处理返回的结果
                 // console.log("接收到服务器返回的数据");
-                sucess(xmlhttp);
-            } else {
-                // console.log("对不起");
-                error(xmlhttp);
+                option.success(xmlhttp);
+            }else{
+                // console.log("没有接收到服务器返回的数据");
+                option.error(xmlhttp);
             }
-
         }
     }
-    //判断外界是否传入了超时时间
-    if (timeout){
+    // 判断外界是否传入了超时时间
+    if(option.timeout){
         timer = setInterval(function () {
             console.log("中断请求");
-            //中断请求
             xmlhttp.abort();
-            //关闭计时器
             clearInterval(timer);
-        }, timeout)
+        }, option.timeout);
     }
 }
